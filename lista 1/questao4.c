6120a2370a95338cbc59a1bf1056cf0bc3a03550
aureliusn12 <163034788+aureliusn12@users.noreply.github.com>
2024-05-02 19:23:31 -0300

    terminei e ajeitei a quarta questao

--- lista 1/questao4.c
+++ lista 1/questao4.c
@@ -1,27 +1,33 @@
-/*Ler quatro valores numéricos inteiros e apresentar o resultado dois a dois da adição e multiplicação entre os
-valores lidos, baseando-se na utilização do conceito de propriedade distributiva. Dica: se forem lidas as variáveis
-A, B, C e D, devem ser somados e multiplicados os valores de A com B, A com C e A com D; depois B com C, B
-com D e por último C com D. Note que para cada operação serão utilizadas seis combinações. Assim sendo,
-devem ser realizadas doze operações de processamento, sendo seis para as adições e seis para as multiplicações. */
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int numero1, numero2, numero3, numero4, resultado;
+int main() {
 
-int main (){
-    printf("Insira o 1 valor inteiro: \n");
-    scanf("%d", &numero1);
-    printf("Insira o 2 valor inteiro: \n");
-    scanf("%d", &numero2);
-    printf("Insira o 3 valor inteiro: \n");
-    scanf("%d", &numero3);
-    printf("Insira o 4 valor inteiro: \n");
-    scanf("%d", &numero4);
+int a, b, c, d;
 
-    int soma = numero1+numero2; numero1+numero3; numero1+numero4; numero2+numero3; numero2+numero4; numero3+numero4;
-    int multiplicacao = numero1*numero2; numero1*numero3; numero1*numero4; numero2*numero3; numero2*numero4; numero3*numero4;
+int ab, ac, ad, bc, bd, cd; 
 
-    printf("Os resultados sao:%d %d",soma, multiplicacao);
+printf("Digite quatro números inteiros:\n");
 
+scanf("%d %d %d %d", &a, &b, &c, &d);
+
+ab = a + b;
+ac = a + c;
+ad = a + d;
+bc = b + c;
+bd = b + d;
+cd = c + d;
+
+printf("Resultados:\n");
+printf("%d + %d = %d\n", a, b, ab);
+printf("%d + %d = %d\n", a, c, ac);
+printf("%d + %d = %d\n", a, d, ad);
+printf("%d + %d = %d\n", b, c, bc);
+printf("%d + %d = %d\n", b, d, bd);
+printf("%d + %d = %d\n", c, d, cd);
+printf("%d x %d = %d\n", a, b, a*b);
+printf("%d x %d = %d\n", a, c, a*c);
+printf("%d x %d = %d\n", a, d, a*d);
+printf("%d x %d = %d\n", b, c, b*c);
+printf("%d x %d = %d\n", b, d, b*d);
+printf("%d x %d = %d\n", c, d, c*d);
 }
